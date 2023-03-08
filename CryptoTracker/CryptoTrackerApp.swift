a2724a9905ba457086e5935f6a95cbbc1a7c11d1
Max Valek <valek.maxwell@gmail.com>
2023-03-08 12:19:41 -0600

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Max Valek on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
